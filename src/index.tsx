c4c6f99835924a876058817b8090c3bfe53e5678
Kseng <t-kseng@egencia.com>
2020-12-24 17:33:08 +0100

    Empty shell

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import Button from 'react-bootstrap/Button'
+
+
+ReactDOM.render(<h1>This is a pokemon fetch</h1>, document.getElementById('root'))
